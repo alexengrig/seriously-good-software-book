1ab4c67f64bcd71e6e0109de9277821d35f7e280
Grig Alex <alexengrigdev@gmail.com>
2021-07-31 00:59:27 +0300

    Create Speed3Container with test

---
+++ src/main/java/dev/alexengrig/seriouslygoodsoftwarebook/Speed3Container.java
@@ -0,0 +1,49 @@
+package dev.alexengrig.seriouslygoodsoftwarebook;
+
+public class Speed3Container implements Container<Speed3Container> {
+    private Speed3Container parent = this;
+    private int size = 1;
+    private double amount;
+
+    @Override
+    public void connectTo(Speed3Container other) {
+        Speed3Container root1 = findRootAndCompress();
+        Speed3Container root2 = other.findRootAndCompress();
+        if (root1 == root2) {
+            return;
+        }
+        int size1 = root1.size;
+        int size2 = root2.size;
+        double total1 = size1 * root1.amount;
+        double total2 = size2 * root2.amount;
+        double newAmount = (total1 + total2) / (size1 + size2);
+        if (size1 <= size2) {
+            root1.parent = root2;
+            root2.amount = newAmount;
+            root2.size += size1;
+        } else {
+            root2.parent = root1;
+            root1.amount = newAmount;
+            root1.size += size2;
+        }
+    }
+
+    @Override
+    public void addWater(double amount) {
+        Speed3Container root = findRootAndCompress();
+        root.amount += amount / root.size;
+    }
+
+    @Override
+    public double getAmount() {
+        Speed3Container root = findRootAndCompress();
+        return root.amount;
+    }
+
+    private Speed3Container findRootAndCompress() {
+        if (parent != this) {
+            parent = parent.findRootAndCompress();
+        }
+        return parent;
+    }
+}
---
+++ src/test/java/dev/alexengrig/seriouslygoodsoftwarebook/Speed3ContainerTest.java
@@ -0,0 +1,8 @@
+package dev.alexengrig.seriouslygoodsoftwarebook;
+
+class Speed3ContainerTest extends ContainerTest<Speed3Container> {
+    @Override
+    protected Speed3Container createContainer() {
+        return new Speed3Container();
+    }
+}

24751a7f4cdb264234f835228750b988349de6e5
Grig Alex <alexengrigdev@gmail.com>
2021-07-30 23:11:21 +0300

    Create ReferenceContainerTest

---
+++ src/test/java/dev/alexengrig/seriouslygoodsoftwarebook/ReferenceContainerTest.java
@@ -0,0 +1,8 @@
+package dev.alexengrig.seriouslygoodsoftwarebook;
+
+class ReferenceContainerTest extends ContainerTest<ReferenceContainer> {
+    @Override
+    protected ReferenceContainer createContainer() {
+        return new ReferenceContainer();
+    }
+}
